5d28b4fb2e718941e6b5814d290229edf6670472
takumhonde9 <tmhonde45@gmail.com>
2020-11-11 22:35:51 -0800

    add routes for album, artist and track endpoint

--- final/routes/track.js
+++ final/routes/track.js
@@ -3,8 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 // add route to get all tracks
+// add corresponding controller
 router.get("/tracks", getAllTracks);
 
-router.get("/track/:id", getTrackById);
+// add route to get a track by id
+// add corresponding controller
+router.get("/tracks/:id", getTrackById);
+
+// add route to delete a track by id
+// add corresponding controller
+router.delete("/tracks/:id", deleteTrackById);
 
 module.exports = router;
