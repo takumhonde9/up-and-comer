08fc6908de051926c0e68e8a964abd10b2d84289
takumhonde9 <tmhonde45@gmail.com>
2020-11-12 00:30:31 -0800

    add comments to note name of controller

--- final/routes/track.js
+++ final/routes/track.js
@@ -4,14 +4,17 @@ const router = express.Router();
 
 // add route to get all tracks
 // add corresponding controller
+// controller: getAllTracks
 router.get("/tracks", getAllTracks);
 
 // add route to get a track by id
 // add corresponding controller
+// controller: getTrackById
 router.get("/tracks/:id", getTrackById);
 
 // add route to delete a track by id
 // add corresponding controller
-router.delete("/tracks/:id", deleteTrackById);
+// controller: getDeleteTrackById
+router.delete("/tracks/:id", getDeleteTrackById);
 
 module.exports = router;
