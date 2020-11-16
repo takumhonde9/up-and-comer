abce4d8a3800855ccb530d7c5ae57b041b237d70
takumhonde9 <tmhonde45@gmail.com>
2020-11-16 11:28:26 -0800

    import routes

--- final/app.js
+++ final/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const { port } = require("./config");
+const { album, artist, track } = require("./routes");
+const {
+  ENDPOINT_ALBUM,
+  ENDPOINT_TRACK,
+  ENDPOINT_ARTIST,
+} = require("./constants");
 
 // create instance of app
 const app = express();
+
+// set up routes
+app.use(ENDPOINT_TRACK, track);
+app.use(ENDPOINT_ARTIST, artist);
+app.use(ENDPOINT_ALBUM, album);
 
 // start up server
 app.listen(port, () => {
