0049ffae3ebfd048a857478f07a507046bb88d98
takumhonde9 <tmhonde45@gmail.com>
2020-11-16 17:19:48 -0800

    add root path to directory

--- final/constants.js
+++ final/constants.js
@@ -1,6 +1,7 @@
 exports.ENDPOINT_ALBUM = "/albums";
 exports.ENDPOINT_TRACK = "/tracks";
 exports.ENDPOINT_ARTIST = "/artists";
+exports.ROOT_PATH = __dirname;
 exports.HTTP_STATUS = {
   OK: 200,
   SERVER_ERROR: 500,
